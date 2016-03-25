e9f9d84cacfc0d89e2a72cc9589835a58d5eaaa8
zhenhou <zhn.hou@gmail.com>
2016-03-24 21:23:34 -0500

    minor

--- hpylib/cosmology/read_spt_data.py
+++ hpylib/cosmology/read_spt_data.py
@@ -17,9 +17,9 @@ def read_spt_bandpower(path):
     nm_ps = 19.3
     nm_cl = 5.0
 
-    dls_sz = np.loadtxt(path+'/fg/dl_shaw_tsz_s10_153ghz.txt', dtype=np.float64, usecols=1)
-    dls_ps = np.loadtxt(path+'/fg/dl_poisson_point_source.txt', dtype=np.float64, usecols=1)
-    dls_cl = np.loadtxt(path+'/fg/dl_0p8', dtype=np.float64, usecols=1)
+    dls_sz = np.loadtxt(path+'/fg/dl_shaw_tsz_s10_153ghz.txt', dtype=np.float64, usecols=[1])
+    dls_ps = np.loadtxt(path+'/fg/dl_poisson_point_source.txt', dtype=np.float64, usecols=[1])
+    dls_cl = np.loadtxt(path+'/fg/dl_0p8', dtype=np.float64, usecols=[1])
     
     tmp = np.loadtxt(path+'windows/window_1', dtype=np.float64)
     winminell = int(tmp[0,0])
@@ -29,7 +29,7 @@ def read_spt_bandpower(path):
     
     winfunc = np.zeros(winmaxell-winminell+1,num_band)
     for i in np.arange(num_band):
-        tmp = np.loadtxt(path+'windows/window_'+str(i+1), dtype=np.float64, usecols=1)
+        tmp = np.loadtxt(path+'windows/window_'+str(i+1), dtype=np.float64, usecols=[1])
         winfunc[:,i] = tmp
         dbs_fgsub[i] = dbs[i] - sum(winfunc[:,i]*dls_fg)
 
