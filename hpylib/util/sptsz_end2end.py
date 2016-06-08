a0743875132b02636bfe9d0b19a4f493f21b027b
zhenhou <zhn.hou@gmail.com>
2016-06-08 11:25:08 -0500

    add ellmin and ellmax

--- hpylib/util/sptsz_end2end.py
+++ hpylib/util/sptsz_end2end.py
@@ -2,15 +2,22 @@ import numpy as np
 from scipy.io.idl import readsav
 from sys import exit
 
-def restore_end_save(savfile):
+def restore_end_save(savfile, ellmin=None, ellmax=None):
 
     n = readsav(savfile)
     key = n.keys()
     if (len(key) != 1):
         exit(".sav file is not a combined end2end file")
 
-    num_bands = int(n[key[0]]['num_bands'][0])
     bands = n[key[0]]['bands'][0]
+
+    if (not ellmin):
+        ellmin = bands[0]
+
+    if (not ellmax):
+        ellmax = bands[-1]
+
+    num_bands = int(n[key[0]]['num_bands'][0])
     dbs_data = n[key[0]]['dbs_data_combined'][0]
     dbs_sims = n[key[0]]['dbs_sims_combined'][0] # (nsims, nspecs, nbands)
 
